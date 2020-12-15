524e6bc808569af61d9f5fec745bece9c6a63908
Jang Rush <weakish@gmail.com>
2020-12-15 22:37:03 +0800

    :memo: IsUnixHiddenFile

--- unix.go
+++ unix.go
@@ -2,6 +2,7 @@ package gosugar
 
 import "strings"
 
+// IsUnixHiddenFile detects if a file name indicating it is a unix hidden file.
 func IsUnixHiddenFile(name string) bool {
 	if name == "." || name == ".." {
 		return false
